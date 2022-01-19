01601b778dad716f14aa5f7af23a6691c8c79a86
RebeccaColquhoun <56828199+RebeccaColquhoun@users.noreply.github.com>
2022-01-19 15:48:11 +0000

    smaller bins

--- code/delaytime_smaller_bins.py
+++ code/delaytime_smaller_bins.py
@@ -13,20 +13,134 @@ root = '/home/earthquakes1/homes/Rebecca/phd/data/2019_global_m5/'
 eq_list = os.listdir(root)
 
 window = 2000 
-df = pd.DataFrame({'5.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
-                   '6.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
-                   '7.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)]})
-counts = pd.DataFrame({'5.0':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                       '6.0':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                       '7.0':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]})
+df = pd.DataFrame({'5.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.1':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.2':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.3':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.4':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.5':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.6':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.7':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.8':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.9':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.1':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.2':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.3':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.4':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.5':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.6':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.7':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.8':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.9':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '7.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window),
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)]})
+counts = pd.DataFrame({'5.0':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.1':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.2':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.3':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.4':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.5':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.6':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.7':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.8':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '5.9':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.0':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.1':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.2':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.3':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.4':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.5':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.6':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.7':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.8':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '6.9':[0, 0, 0, 0, 0, 0, 0, 0],
+                       '7.0':[0, 0, 0, 0, 0, 0, 0, 0]})
 
-aad = pd.DataFrame({'5.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
-                   '6.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
-                   '7.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)]})
+aad = pd.DataFrame({'5.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.1':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.2':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.3':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.4':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.5':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.6':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.7':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.8':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '5.9':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.1':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.2':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.3':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.4':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.5':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.6':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.7':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.8':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '6.9':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window), 
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)],
+                   '7.0':[np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window),
+                          np.zeros(window), np.zeros(window), np.zeros(window), np.zeros(window)]})
 
-ad = pd.DataFrame({'5.0':[[], [], [], [], [], [], [], [], [], [], [], []],
-                   '6.0':[[], [], [], [], [], [], [], [], [], [], [], []],
-                   '7.0':[[], [], [], [], [], [], [], [], [], [], [], []]})
+ad = pd.DataFrame({'5.0':[[], [], [], [], [], [], [], []],
+                   '5.1':[[], [], [], [], [], [], [], []],
+                   '5.2':[[], [], [], [], [], [], [], []],
+                   '5.3':[[], [], [], [], [], [], [], []],
+                   '5.4':[[], [], [], [], [], [], [], []],
+                   '5.5':[[], [], [], [], [], [], [], []],
+                   '5.6':[[], [], [], [], [], [], [], []],
+                   '5.7':[[], [], [], [], [], [], [], []],
+                   '5.8':[[], [], [], [], [], [], [], []],
+                   '5.9':[[], [], [], [], [], [], [], []],
+                   '6.0':[[], [], [], [], [], [], [], []],
+                   '6.1':[[], [], [], [], [], [], [], []],
+                   '6.2':[[], [], [], [], [], [], [], []],
+                   '6.3':[[], [], [], [], [], [], [], []],
+                   '6.4':[[], [], [], [], [], [], [], []],
+                   '6.5':[[], [], [], [], [], [], [], []],
+                   '6.6':[[], [], [], [], [], [], [], []],
+                   '6.7':[[], [], [], [], [], [], [], []],
+                   '6.8':[[], [], [], [], [], [], [], []],
+                   '6.9':[[], [], [], [], [], [], [], []],
+                   '7.0':[[], [], [], [], [], [], [], []]})
 
 client = Client("IRIS")
 # cat = client.get_events(starttime=UTCDateTime("2019-06-26"), endtime=UTCDateTime("2020-06-26"), minlongitude=-179, maxlongitude=-145, minlatitude=42, maxlatitude=71, minmagnitude=5, includearrivals=True)
@@ -51,6 +165,7 @@ for eq_name in eq_with_data:
     eq_lat= cat_entry.origins[0].latitude
     eq_long = cat_entry.origins[0].longitude
     eq_mag = cat_entry.magnitudes[0].mag
+    eq_mag_str = "{:.1f}".format(eq_mag)
 
     data = obspy.read(root+eq_name+'/data/*/*')
     data.resample(20)
@@ -59,7 +174,7 @@ for eq_name in eq_with_data:
     with open(root+eq_name+'/picks.pkl', 'rb') as f:
         picks = pickle.load(f)
     n_records = 0
-    sampling_rate = 50 # get this from tr.stats.sampling_rate
+    sampling_rate = 20 # get this from tr.stats.sampling_rate
     # sensor_types = self.data_stats['sensor_types'] for automated OOP
     
     for i in range(0, len(data)):  # iterate through all traces
@@ -76,33 +191,35 @@ for eq_name in eq_with_data:
                 sta_long = inv.select(network = tr.stats.network, station = tr.stats.station)[0][0].longitude
 
                 distance = np.sqrt((eq_lat - sta_lat)**2 + (eq_long - sta_long)**2) * 110 # 2D for now...
-                dists.append(distance)
-                print(distance)
-                tr.filter('bandpass', freqmin=0.1, freqmax = 3) 
-                displ = tr.integrate()
-                abs_displ = abs(displ.data) # find absolute of trace
-                sum_abs_displ = df[str(np.floor(eq_mag))][int(distance//25)]
-                window = int(100 * tr.stats.sampling_rate)
-                sum_abs_displ =  sum_abs_displ + abs_displ[pick_samples:pick_samples+window] # calculate aad for 10000 samples after p wave arrival
-                df[str(np.floor(eq_mag))][int(distance//25)] = sum_abs_displ
-                current = ad[str(np.floor(eq_mag))][int(distance//25)]
-                current.append(abs_displ[pick_samples:pick_samples+window])
-                ad[str(np.floor(eq_mag))][int(distance//25)]= current
-                counts[str(np.floor(eq_mag))][int(distance//25)] = counts[str(np.floor(eq_mag))][int(distance//25)]  + 1
+                if distance < 200: 
+                    dists.append(distance)
+                    print(distance)
+                    tr.filter('bandpass', freqmin=0.1, freqmax = 3) 
+                    displ = tr.integrate()
+                    abs_displ = abs(displ.data) # find absolute of trace
+                    sum_abs_displ = df[eq_mag_str][int(distance//25)]
+                    window = int(100 * tr.stats.sampling_rate)
+                    sum_abs_displ =  sum_abs_displ + abs_displ[pick_samples:pick_samples+window] # calculate aad for 10000 samples after p wave arrival
+                    df[eq_mag_str][int(distance//25)] = sum_abs_displ
+                    current = ad[eq_mag_str][int(distance//25)]
+                    current.append(abs_displ[pick_samples:pick_samples+window])
+                    ad[eq_mag_str][int(distance//25)]= current
+                    counts[eq_mag_str][int(distance//25)] = counts[str(np.floor(eq_mag))][int(distance//25)]  + 1
             except:
                 continue
             
         # n_records += 1
 
     '''aad = sum_abs_displ/n_records'''
-fig, axs = plt.subplots(4, df.shape[1]-1)
+fig, axs = plt.subplots(8, df.shape[1]-1)
 print('onto row/column plot')
 row_count = 0
-for row in range(4):
-    print('for row')
+results = []
+for row in range(8):
+    #print('for row')
     col_count = 0
     for column in df.columns[:-1]:
-        print('for column')
+        #print('for column')
         aad[column][row]=df[column][row]/counts[column][row]
         aad_bin = aad[column][row]
         # now to find where departure delay exceeds DPD
@@ -114,9 +231,9 @@ for row in range(4):
         delay_time = []
         decline = 0 # count how many amplitudes have decreased in a row
         for point in range(1,len(aad_bin)):
-            print('for point in aad')
+            #print('for point in aad')
             if aad_bin[point]<aad_bin[point-1]:
-                print('less than previous point')
+                #print('less than previous point')
                 decline += 1
                 if decline == np.ceil(DPD_samples)-1: #if surpassed the DPD
                     delay_time.append(point)
@@ -126,7 +243,12 @@ for row in range(4):
         if len(T_dp)>0:
             for i in range(0, min(5, len(T_dp))): # vertical lines marking potential Tdp locations (first 5 after P wave pick)
                 axs[row_count][col_count].vlines(T_dp[i], min(aad_bin[0:250]), max(aad_bin[0:250]), zorder = 100)
-
+            if float("{:.2f}".format(T_dp[0]))!=0.05:
+                print(column, row_count, T_dp[0])
+                results.append([column, row_count, T_dp[0]])
+            elif len(T_dp)>1:
+                print(column, row_count, T_dp[1])
+                results.append([column, row_count, T_dp[1]])
         axs[row_count][col_count].plot(np.arange(0, 5, 0.02), aad[column][row][:250], zorder = 50, color = 'red')
         for ind_ad in ad[column][row]:
             axs[row_count][col_count].plot(np.arange(0, 5, 0.02), ind_ad[:250], color='lightgrey')
